90c71557fe92d0986b9ed73678554185acc593b4
NextFEM <info@nextfem.it>
2024-11-23 10:39:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='NextFEMpy',
+    name='nextfempy',
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -15,9 +15,9 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/NextFEM/NextFEMpy',
+    license="MIT",
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSS Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
 )
